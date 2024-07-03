2fc25dddf8cb5ec7a0aeea136b40b5f592219fff
周杨洋 <969620451@qq.com>
2024-07-03 16:08:31 +0800

    ci: 修改prettier规则

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,23 @@
+// module.exports = {
+//   tabWidth: 2,
+//   semi: true,
+//   printWidth: 80,
+//   singleQuote: true,
+//   quoteProps: 'consistent',
+//   htmlWhitespaceSensitivity: 'strict',
+//   vueIndentScriptAndStyle: true,
+// };
 module.exports = {
   tabWidth: 2,
-  semi: true,
+  useTabs: false,
+  semi: false,
+  arrowParens: 'avoid',
   printWidth: 80,
   singleQuote: true,
   quoteProps: 'consistent',
   htmlWhitespaceSensitivity: 'strict',
   vueIndentScriptAndStyle: true,
-};
+  endOfLine: 'auto',
+  trailingComma: 'all',
+  bracketSpacing: true,
+}
